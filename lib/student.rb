49e0947672dea8a8b46853d39247ebb2a9bc51b5
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-09-30 04:40:42 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, grade, id=nil)
+  def initialize(id=nil, name, grade)
     @id = id
     @name = name
     @grade = grade
@@ -35,7 +35,7 @@ class Student
       self.update
     else
       sql = <<-SQL
-        INSERT INTO students (name, grade)
+        INSERT INTO students(name, grade)
         VALUES (?, ?)
         SQL
 
@@ -52,7 +52,7 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = self.new(row[1], row[2], row[0])
+    new_student = self.new(row[0], row[1], row[2])
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
