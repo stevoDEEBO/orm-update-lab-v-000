f60b0ed4be84e1684646c139307563052a1d943b
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-09-30 04:52:14 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
       sql = <<-SQL
         INSERT INTO students(name, grade)
         VALUES (?, ?)
-        SQL
+      SQL
 
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
@@ -52,11 +52,8 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = self.new(row[0], row[1], row[2])
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    student = self.new(row[0], row[1], row[2])
+    student
   end
 
   def self.find_by_name(name)
